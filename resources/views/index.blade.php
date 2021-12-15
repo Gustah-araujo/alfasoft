7b4edd1d008767bae9d9bd2ab11ee6e7886d78e0
gustah-araujo <gustavo.araujo2001@yahoo.com.br>
2021-12-15 09:44:19 -0300

    Template front-end importado

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.index')
+@section('meta')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <div class="main-container">
+        <h1>Lista de contatos</h1>
+        <h2>Aqui poderá ver a lista de todos os usuários cadastrados no banco de dados</h2>
+    </div>
+@endsection

fb411a96935f9b7034fffdef338f4dd3eed65395
gustah-araujo <gustavo.araujo2001@yahoo.com.br>
2021-12-15 11:15:48 -0300

    Método Read(Index) implementado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,5 +7,31 @@
     <div class="main-container">
         <h1>Lista de contatos</h1>
         <h2>Aqui poderá ver a lista de todos os usuários cadastrados no banco de dados</h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Telefone</td>
+                    <td>Email</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($contacts as $c)
+                    <tr>
+                        <td>{{ $c->id }}</td>
+                        <td>{{ $c->name }}</td>
+                        <td>{{ $c->contact }}</td>
+                        <td>{{ $c->email }}</td>
+                        <td class="btn-td"><button class="edit-btn">Editar</button></td>
+                        <td class="btn-td"><button class="del-btn">X</button></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
